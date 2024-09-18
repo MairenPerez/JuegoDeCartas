13f7154108b6653fea87f59b756ff38198e64a31
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-09-18 09:51:37 +0200

    Selección de jugadores en la partida

--- JuegoDeCartas/Program.cs
+++ JuegoDeCartas/Program.cs
@@ -10,12 +10,34 @@ namespace JuegoDeCartas
     {
         static void Main(string[] args)
         {
-            // Inicializamos una carta
-            Carta carta = new Carta(new Random().Next(1, 13), Carta.Palo.Copas);
-            Console.ReadKey();
+            Console.WriteLine("Bienvenido al Juego de Cartas!");
+            int numJugadores = SeleccionarNumJugadores();
 
-            // Mostramos la carta
-            
+            Baraja baraja = new Baraja();
+
+            Console.WriteLine("El juego ha comenzado con " + numJugadores + " jugadores.");
+        }
+
+        /// <summary>
+        /// Selección de jugadores
+        /// que intervendrán en la 
+        /// partida
+        /// </summary>
+        /// <returns>numJugadores</returns>
+        private static int SeleccionarNumJugadores()
+        {
+            int numJugadores = 0;
+
+            while (numJugadores < 2 || numJugadores > 5)
+            {
+                Console.WriteLine("Selecciona el número de jugadores (2-5) ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out numJugadores) && numJugadores >= 2 && numJugadores <= 5)
+                    return numJugadores;
+                else
+                    Console.WriteLine("Por favor, introduce un número válido de jugadores.");
+            }
+            return numJugadores;
         }
     }
 }
